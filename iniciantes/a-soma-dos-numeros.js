284014fe3a60424a6bf11021af295e5f9b63c6aa
Fransuelton <elton6103@gmail.com>
2024-11-27 23:28:33 -0300

    ✨ feat: add challenge for summing numbers from 1 to 5

---
+++ iniciantes/a-soma-dos-numeros.js
@@ -0,0 +1,14 @@
+/*
+Desafio 4: "A Soma dos Números" ➕
+Objetivo: Somar os números de 1 a 5.
+Instruções:
+Use um laço de repetição (for) para somar os números de 1 a 5.
+Armazene o resultado em uma variável chamada soma.
+Mostre no console o resultado final da soma.
+Recompensa: +25 XP por praticar somas e laços juntos.
+*/
+
+for (let i = 1; i <= 5; i++) {
+  const soma = i + i;
+  console.log(soma);
+}

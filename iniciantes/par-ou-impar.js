44bbddb648f38a50c4f461df6f23732181ea0893
Fransuelton <elton6103@gmail.com>
2024-11-27 23:22:45 -0300

    ✨ feat: add challenge to identify even or odd numbers

---
+++ iniciantes/par-ou-impar.js
@@ -0,0 +1,23 @@
+/*
+
+Desafio 1: "Par ou Ímpar?" ⚖️
+
+Objetivo: Identificar se um número é par ou ímpar.
+
+Instruções:
+
+Crie uma variável chamada numero e atribua a ela um número qualquer.
+Use um operador lógico para verificar se o número é par (divisível por 2).
+Exiba no console: "O número é par" ou "O número é ímpar".
+
+Recompensa: +10 XP por aprender a usar operadores matemáticos e condicionais.
+
+*/
+
+const numero = 1;
+
+if (numero % 2 === 0) {
+  console.log("O número é par");
+} else {
+  console.log("O número é impar");
+}

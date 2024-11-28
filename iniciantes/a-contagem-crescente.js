ceb167051d3b1fe22c59296d560630161f9c2fac
Fransuelton <elton6103@gmail.com>
2024-11-27 23:22:59 -0300

    ✨ feat: add challenge for counting from 1 to 10

---
+++ iniciantes/a-contagem-crescente.js
@@ -0,0 +1,16 @@
+/*
+Desafio 2: "A Contagem Crescente" 🔢
+
+Objetivo: Criar uma sequência de números de 1 a 10.
+
+Instruções:
+
+Use um laço de repetição (for) para contar de 1 a 10.
+Mostre cada número no console.
+
+Recompensa: +15 XP por entender a lógica de laços básicos.
+*/
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
